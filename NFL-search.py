47e3c668dd8d49679fe95a0dda9ae1035737f2b4
Colton Almaraz <almarazcolton333@gmail.com>
2023-06-14 10:44:23 -0700

    Updated

--- NFL-search.py
+++ NFL-search.py
@@ -1,81 +1,176 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup, Comment
 import requests
 import csv
-import re
+import time
 
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+base_url = "https://www.pro-football-reference.com"
 
-def gameLog(player_link):    #Find the "Game Logs" section of the page and check if last game played was 2018 or earlier
-    website_url = requests.get(player_link).text
-    soup = BeautifulSoup(website_url,'lxml')
-    gamelog = soup.find(text='Gamelogs').findNext('ul')
-    games = gamelog.find_all('li')
-    if int(games[-1].text) < 2019: #year
-        return games[-1].text
-    else:
-        return False
+def writePlayer(params):
+    max_transactions = params[1]
+    players = params[0]
 
-    
-def writePlayer(list_of_players): #Write players to file called "fooball_players_years_over_3.csv"
-    with open('fooball_players_years_over_3.csv', 'w',encoding='utf-8', newline='') as file:
+    with open('football_players.csv', 'w',encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
-        for item in list_of_players:
-            writer.writerow(item)
-        file.close()
 
+        headers = ['Name', 'DOB', 'Birth Place', 'College', 'High School'] + \
+                  ['Transaction {}'.format(i) for i in range(1, max_transactions+1)] + \
+                  ['Teams by Year', 'Number of Years Played', 'List of Years Played']
+        writer.writerow(headers)
+
+        for player in players.values():
+            row = [player['name'], player['dob'], player['birthplace'], player['college'], player['high_school']] + \
+                  player['transactions'] + \
+                  ([''] * (max_transactions - len(player['transactions']))) + \
+                  [player['teams'], player['num_years_played'], player['years_played']]
+            writer.writerow(row)
 
 def getPlayers():
-    players_names=[]
-    players_list=[]
-    website_url = requests.get("https://www.pro-football-reference.com/teams/").text  #Initial site
-    soup = BeautifulSoup(website_url,'lxml')
-    teams_table = soup.find_all('tbody')
-    team_names = teams_table[0].find_all("a", href=re.compile(r"^/teams"))  #Find all links that link to a team page 
-    for team_name in team_names:
-        website_url = requests.get("https://www.pro-football-reference.com" + team_name['href']).text #Get team page of each active team
-        soup = BeautifulSoup(website_url,'lxml')
-        team = team_name.text
-        print(team) #Prints team name to keep track of how far the program is through the list 
-        team_years = soup.find_all("th", {"data-stat": "year_id"}) #Find all years team played 
-        for year_link in team_years:
-            if year_link.text != 'Year' and 2005 <= int(year_link.text) <= 2017: #Loop though all years 2005-2017
-                print(year_link.text) #Prints year to keep track of how far the program is through the team
-                website_url = requests.get("https://www.pro-football-reference.com"+team_name['href']+year_link.text+"_roster.htm").text
-                soup = BeautifulSoup(website_url,'lxml')
-                comments = soup.findAll(text=lambda text:isinstance(text, Comment))
-                for comment in comments:
-                    if 'table' in comment:
-                        comment_soup = BeautifulSoup(comment, 'html.parser')
-                        for tr in comment_soup.find_all('tr'):
-                            td = tr.find_all('td')
-                            if len(td) > 0:
-                                try:
-                                    name = td[0].text
-                                    for letter in name:
-                                        if letter == '+' or letter == '*':
-                                            name = name.replace(letter, '')
-                                    if td[9].text != "Rook" and int(td[9].text) >= 3 and name not in players_names: #Find players who have played >3 years 
-                                        players_names.append(name)
-                                        last_game_year = gameLog("https://www.pro-football-reference.com"+td[0].find('a')['href'])
-                                        if last_game_year:
-                                            player = []
-                                            player.extend((td[0].text,td[8].text,team))
-                                            players_list.append(player)
-                                except:
-                                    pass
-    return players_list
-                                    
+    players_hash={}
 
-                    
-                    
+    for i in alphabet:
+        print(f"Processing players starting with {i}...")
+        if i == "B":
+            break;
+
+        website_url = requests.get(base_url + "/players/"+i+"/").text
+        soup = BeautifulSoup(website_url,'lxml')
+        player_table = soup.find("div", {"id": "div_players"})
+        players_in_player_table = player_table.find_all("p")
+        j = 0;
+        highest_num_transactions = 0
+
+        for player in players_in_player_table:
+            total_transactions = 0
+            j += 1
+
+            if j == 20:
+                break;
+            try:
+                time.sleep(2)
+
+                start_year, end_year = player.text.split(" ")[-1].split("-")
+
+                if int(end_year) < 1990:
+                    continue;
+
+                # YEARS PLAYED
+                years_played = int(end_year) - int(start_year) + 1
+                # PLAYER NAME
+                player_name = player.find("a").text
+                print(f"Processing player: {player_name}. {round((j / len(players_in_player_table)) * 100, 2)}% done with {i}")
+
+                player_link = player.find("a").get('href')
+                player_link_res = requests.get(base_url + player_link).text
+
+                player_soup = BeautifulSoup(player_link_res,'lxml')
+                player_info_container = player_soup.find("div", { "id":"meta" })
+                player_dob_container = player_info_container.find("span", {"id":"necro-birth"}).parent
                 
-            
+                try:
+                    # PLAYER DOB
+                    player_dob = player_info_container.find("span", {"id":"necro-birth"}).get("data-birth")
+                except Exception as e:
+                    player_dob = ""
+
+                try:
+                    # PLAYER BIRTHPLACE
+                    player_dob_container_children = list(player_dob_container.children)
+                    elems = [str(elem) for elem in player_dob_container_children if elem is not None]
+                    html_string = ''.join(elems)
+                
+                    birthplace_soup = BeautifulSoup(html_string,'lxml')
+                    birthplace_spans =  birthplace_soup.find_all("span")
+                    player_birth_place = birthplace_spans[1].text.replace('\xa0', ' ').replace('in', '').strip().split(',')
+                    
+                except Exception as e:
+                    player_birth_place = ""
+
+                p_tags = player_info_container.find_all('p')
+
+                player_college = ''
+                player_high_school = ''
+
+                for p in p_tags:
+                    if p.text.startswith('College'):
+                        # PLAYER COLLEGE
+                        player_college = p.a.text 
+                    elif p.text.startswith('High School'):
+                        # PLAYER HIGH SCHOOL
+                        player_high_school = p.a.text  
+
+                try:
+                    html_player_soup = BeautifulSoup(player_link_res,'html.parser')
+
+                    comments = html_player_soup.find_all(string=lambda text: isinstance(text, Comment))
+                    for comment in comments:
+                        # Create a soup object from the comment string
+                        comment_soup = BeautifulSoup(comment, 'html.parser')
+
+                        # Now you can find elements within the comment just like you would in a normal soup object
+                        div = comment_soup.find('div', {'class': 'section_content', 'id': 'div_transactions'})
+                        if div is not None:
+                            li_tags = div.find_all('li')
+                            transactions = []
+                            for li in li_tags:
+                                if li.text.split()[0] != "Show":
+                                    transactions.append(li.text)
+                                    total_transactions += 1
+                            # PLAYER TRANSACTIONS
+                            player_transactions = transactions
+                            if total_transactions > highest_num_transactions:
+                                highest_num_transactions = total_transactions
+
+                except Exception as e:
+                    player_transactions = ""
+
+                player_teams = {}
+                player_years_played = []
+
+                try:
+                    career_game_log_link = player_soup.find('a', string='Career').get('href')
+                    career_game_log_link_res = requests.get(base_url + career_game_log_link).text
+
+                    game_log_soup = BeautifulSoup(career_game_log_link_res,'lxml')
+                    table = game_log_soup.find('table', {'id': 'stats'})
+                    rows = table.tbody.find_all('tr')
+
+                    for row in rows:
+                        try:
+                            year = row.find('td', {'data-stat': 'year_id'}).text
+                            if year in player_teams:
+                                continue
+                            else:
+                                player_teams[year] = row.find('td', {'data-stat': 'team'}).text
+                                player_years_played.append(year)
+                        except:
+                            continue
+
+                    # PLAYER TEAMS
+                    player_teams = ', '.join(f"{key}:{value}" for key, value in player_teams.items())
+                except Exception as e:
+                    player_teams = ""
+
+                player_info = {
+                    'name': player_name,
+                    'dob': player_dob,
+                    'birthplace': ", ".join(player_birth_place),
+                    'college': player_college,
+                    'high_school': player_high_school,
+                    'transactions': player_transactions,
+                    'teams': player_teams,
+                    'num_years_played': years_played,
+                    'years_played': ", ".join(player_years_played) if len(player_years_played) > 0 else ""
+                }
+
+                player_birth_place_str = ", ".join(str(bp) for bp in player_birth_place) if player_birth_place is not None else ''
+                players_hash[player_name + str(player_dob) + player_birth_place_str] = player_info
+
+            except Exception as e:
+                print(player, e)
+                continue
+
+    return [players_hash, highest_num_transactions]
+                                    
 writePlayer(getPlayers())
 
-
-        
